87f54fb247f8256cd24b8571c8914e131f2327c6
seoyun <34929351+nominated93@users.noreply.github.com>
2022-01-13 15:55:06 +0900

    Create 나누어_떨어지는_숫자_배열.cpp

---
+++ level_1/나누어_떨어지는_숫자_배열.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    bool hasDiv = false;
+    
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] % divisor == 0){
+            hasDiv = true;
+            answer.push_back(arr[i]);
+        }
+    }
+    
+    if(hasDiv){
+        sort(answer.begin(), answer.end());
+    } else{
+        answer.push_back(-1);
+    }
+        
+    
+    return answer;
+}
